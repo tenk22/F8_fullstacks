4ff98653b8560f90fc7638e398401f427c1c424c
Duongnhhe141263 <80703421+Duongnhhe141263@users.noreply.github.com>
2024-10-12 18:23:06 +0700

    nopbt27

---
+++ exejs/day10/ex1.js
@@ -0,0 +1,10 @@
+// const btnElement = document.querySelector(".btn01");
+// btnElement.style.backgroundColor = "blue";
+// btnElement.style.padding = "12px";
+// btnElement.style.borderRadius = "6px";
+// btnElement.style.cssText = "font-size: 16px; color: red;";
+// console.log(btnElement);
+
+/**
+ * sử dụng javascript để tô nền cho các dòng trong bảng có số thứ tự chẵn thành màu "#ccc"
+ */
