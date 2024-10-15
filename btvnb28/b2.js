aa92cbf8edfb2a2c35b4267e96224c7fb8f6700c
Duongnhhe141263 <80703421+Duongnhhe141263@users.noreply.github.com>
2024-10-15 20:20:12 +0700

    suabt28

--- btvnb28/b2.js
+++ btvnb28/b2.js
@@ -26,11 +26,33 @@ const userInfor = {};
 
 // http://127.0.0.1:5501/15.%20DOM_3/Demo/ex03.html?email=1%40gmail.com&password=1234567890&gender=female
 
+// formElement.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   // Cách 1: Sử dụng vòng lặp để lặp qua từng index của event.target -> lấy name-value đưa vào userInfor
+
+//   // Cách 2: Chọn từng phần tử input của form, sau đó lấy value thông qua element.value
+//   const fullName = document.getElementById("fullname").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+//   const confirmPassword = document.getElementById("confirm_password").value;
+//   const gender = document.getElementById("gender").value;
+
+//   const registerInfo = {
+//     fullName: fullName,
+//     email: email,
+//     password: password,
+//     confirmPassword: confirmPassword,
+//     gender: gender,
+//   };
+
+//   console.log(registerInfo);
+// });
+
+const formElement = document.getElementById("register");
+
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  // Cách 1: Sử dụng vòng lặp để lặp qua từng index của event.target -> lấy name-value đưa vào userInfor
 
-  // Cách 2: Chọn từng phần tử input của form, sau đó lấy value thông qua element.value
   const fullName = document.getElementById("fullname").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
